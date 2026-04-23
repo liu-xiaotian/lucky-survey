ccd29a8634a7899acafca12cb8d8badeff7c7d47
tt <xiaotian3303@outlook.com>
2026-04-23 21:05:41 +0800

    feat: mainlayout index.css

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,13 +1,24 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import { Layout } from 'antd'
+import styles from './MainLayout.module.scss'
+
+const { Header, Content, Footer } = Layout
 
 const MainLayout = () => {
   return (
-    <>
-      <div>MainLayout header</div>
-      <Outlet />
-      <div>MainLayout footers</div>
-    </>
+    <Layout>
+      <Header className={styles.header}>
+        <div className={styles.left}>Logo</div>
+        <div className={styles.right}>登录</div>
+      </Header>
+      <Layout className={styles.main}>
+        <Content>
+          <Outlet />
+        </Content>
+      </Layout>
+      <Footer className={styles.footer}>天天问卷 &copy; 2026 - present. Created by tian</Footer>
+    </Layout>
   )
 }
 
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import { Button } from 'antd'
 
 const Login: React.FC = () => {
-  return <div>Login</div>
+  return (
+    <>
+      <div>Login</div>
+      <Button type="primary">登录</Button>
+    </>
+  )
 }
 
 export default Login
