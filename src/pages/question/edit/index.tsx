e4abe9d5a0b6a0f1e19a4045070bf98405542313
tt <xiaotian3303@outlook.com>
2026-04-23 19:56:46 +0800

    fix: ...

--- src/pages/question/edit/index.tsx
+++ src/pages/question/edit/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
-const index = () => {
-  return <div>index</div>
+const Edit = () => {
+  const { id = '' } = useParams()
+  return <div>index{id}</div>
 }
 
-export default index
+export default Edit
