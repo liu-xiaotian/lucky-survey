34483401aa3bf09edc69bdb7c5d941ec50de4feb
tt <xiaotian3303@outlook.com>
2026-04-23 22:58:32 +0800

    feat: not found 页面

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,7 +1,23 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import { Result, Button } from 'antd'
+import { MANAGE_INDEX_PATHNAME } from '../router'
 
 const NotFound: React.FC = () => {
-  return <div>NotFound</div>
+  const nav = useNavigate()
+
+  return (
+    <Result
+      status="404"
+      title="404"
+      subTitle="抱歉，您访问的页面不存在"
+      extra={
+        <Button type="primary" onClick={() => nav(MANAGE_INDEX_PATHNAME)}>
+          返回首页
+        </Button>
+      }
+    ></Result>
+  )
 }
 
 export default NotFound
