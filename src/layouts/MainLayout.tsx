316cd32cfd01d6b226fc9940f2e40e3bf7c91489
tt <xiaotian3303@outlook.com>
2026-04-23 22:44:56 +0800

    feat: logo

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Outlet } from 'react-router-dom'
 import { Layout } from 'antd'
 import styles from './MainLayout.module.scss'
+import Logo from '../components/Logo'
 
 const { Header, Content, Footer } = Layout
 
@@ -9,7 +10,9 @@ const MainLayout = () => {
   return (
     <Layout>
       <Header className={styles.header}>
-        <div className={styles.left}>Logo</div>
+        <div className={styles.left}>
+          <Logo />
+        </div>
         <div className={styles.right}>登录</div>
       </Header>
       <Layout className={styles.main}>
