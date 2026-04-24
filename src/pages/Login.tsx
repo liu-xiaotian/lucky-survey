91da118675f4647de48f0f7be5cf5aa00863e5c5
tt <xiaotian3303@outlook.com>
2026-04-24 22:16:15 +0800

    feat: login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,12 +1,106 @@
-import React from 'react'
-import { Button } from 'antd'
+import { Link } from 'react-router-dom'
+import { Typography, Space, Form, Input, Button, Checkbox, message } from 'antd'
+import { UserAddOutlined } from '@ant-design/icons'
+import { REGISTER_PATHNAME } from '../router'
+import styles from './Login.module.scss'
+import { useEffect } from 'react'
 
-const Login: React.FC = () => {
+const { Title } = Typography
+
+const USERNAME_KEY = 'USERNAME'
+const PASSWORD_KEY = 'PASSWORD'
+
+function rememberUser(username: string, password: string) {
+  localStorage.setItem(USERNAME_KEY, username)
+  localStorage.setItem(PASSWORD_KEY, password)
+}
+
+function deleteUserFromStorage() {
+  localStorage.removeItem(USERNAME_KEY)
+  localStorage.removeItem(PASSWORD_KEY)
+}
+
+function getUserInfoFromStorage() {
+  return {
+    username: localStorage.getItem(USERNAME_KEY),
+    password: localStorage.getItem(PASSWORD_KEY),
+  }
+}
+// 1. 定义表单值的类型
+interface LoginValues {
+  username: string
+  password: string
+  remember?: boolean
+}
+const Login = () => {
+  // const nav = useNavigate()
+
+  const [form] = Form.useForm() // 第三方 hook
+  useEffect(() => {
+    const { username, password } = getUserInfoFromStorage()
+    form.setFieldsValue({ username, password, remember: !!username }) // 设置表单数据
+  }, [])
+
+  function onFinish(values: LoginValues) {
+    const { username, password, remember } = values || {}
+    if (remember) {
+      rememberUser(username, password)
+    } else {
+      deleteUserFromStorage()
+    }
+    message.success('登录成功')
+  }
+
   return (
-    <>
-      <div>Login</div>
-      <Button type="primary">登录</Button>
-    </>
+    <div className={styles.container}>
+      <div>
+        <Space>
+          <Title level={2}>
+            <UserAddOutlined />
+          </Title>
+          <Title level={2}>用户登录</Title>
+        </Space>
+      </div>
+      <div>
+        <Form
+          labelCol={{ span: 6 }}
+          wrapperCol={{ span: 16 }}
+          initialValues={{ remember: true }}
+          onFinish={onFinish}
+          form={form}
+        >
+          <Form.Item
+            label="用户名"
+            name="username"
+            rules={[
+              { required: true, message: '请输入用户名' },
+              { type: 'string', min: 5, max: 20, message: '字符长度在 5-20 之间' },
+              { pattern: /^\w+$/, message: '只能是字母数字下划线' },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            label="密码"
+            name="password"
+            rules={[{ required: true, message: '请输入密码' }]}
+          >
+            <Input.Password />
+          </Form.Item>
+          <Form.Item name="remember" valuePropName="checked" wrapperCol={{ offset: 6, span: 16 }}>
+            <Checkbox>记住我</Checkbox>
+          </Form.Item>
+          <Form.Item wrapperCol={{ offset: 6, span: 16 }}>
+            <Space>
+              <Button type="primary" htmlType="submit">
+                登录
+              </Button>
+              <Link to={REGISTER_PATHNAME}>注册新用户</Link>
+            </Space>
+          </Form.Item>
+        </Form>
+      </div>
+    </div>
   )
 }
 
