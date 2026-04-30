44bac9d221028ab10f844e8fedfd94f5e5518246
tt <xiaotian3303@outlook.com>
2026-04-30 16:22:33 +0800

    feat: 画布上动态显示组件

--- src/pages/question/edit/EditCanvas.tsx
+++ src/pages/question/edit/EditCanvas.tsx
@@ -1,11 +1,32 @@
 import { Spin } from 'antd'
-import QuestionTitle from '../../../components/QuestionComponents/QuestionTitle/Component'
-import QuestionInput from '../../../components/QuestionComponents/QusetionInput/Component'
+// import QuestionTitle from '../../../components/QuestionComponents/QuestionTitle/Component'
+// import QuestionInput from '../../../components/QuestionComponents/QusetionInput/Component'
 import styles from './EditCanvas.module.scss'
 import useGetComponentInfo from '../../../hooks/useGetComponentInfo'
+import type { ComponentInfoType } from '../../../store/componentsReducer'
+import { getComponentConfByType } from '../../../components/QuestionComponents'
 
 type PropsType = {
   loading: boolean
+}
+
+/**
+ * 根据组件信息生成React组件
+ * @param componentInfo - 组件信息对象，包含组件类型和属性
+ * @returns 返回生成的React组件，如果找不到对应配置则返回null
+ */
+function genComponent(componentInfo: ComponentInfoType) {
+  // 从组件信息中解构出组件类型和属性
+  const { type, props } = componentInfo
+
+  // 根据组件类型获取组件配置
+  const componentConf = getComponentConfByType(type)
+  // 如果没有找到组件配置，则返回null
+  if (componentConf == null) return null
+  // 从组件配置中解构出组件
+  const { Component } = componentConf
+  // 返回渲染后的组件，并将属性传递给组件
+  return <Component {...props} />
 }
 const EditCanvas = ({ loading }: PropsType) => {
   const { componentList } = useGetComponentInfo()
@@ -20,16 +41,14 @@ const EditCanvas = ({ loading }: PropsType) => {
   }
   return (
     <div className={styles.canvas}>
-      <div className={styles['component-wrapper']}>
-        <div className={styles.component}>
-          <QuestionTitle />
-        </div>
-      </div>
-      <div className={styles['component-wrapper']}>
-        <div className={styles.component}>
-          <QuestionInput />
-        </div>
-      </div>
+      {componentList.map(c => {
+        const { fe_id } = c
+        return (
+          <div key={fe_id} className={styles['component-wrapper']}>
+            <div className={styles.component}>{genComponent(c)}</div>
+          </div>
+        )
+      })}
     </div>
   )
 }
