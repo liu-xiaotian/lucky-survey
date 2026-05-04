0ca70fc614e6e350db07d5abbd98c24f47b0850d
tt <xiaotian3303@outlook.com>
2026-05-04 11:32:33 +0800

    feat: 动态切换 tab

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -9,3 +9,16 @@ export const LIST_SEARCH_PARAM_KEY = 'keyword'
 
 export const LIST_PAGE_PARAM_KEY = 'page'
 export const LIST_PAGE_SIZE_PARAM_KEY = 'pageSize'
+
+// TS 枚举
+// enum TAB_KEYS {
+//   PROP_KEY = 'prop',
+//   SETTING_KEY = 'setting',
+// }
+
+export const TAB_KEYS = {
+  PROP_KEY: 'prop',
+  SETTING_KEY: 'setting',
+} as const
+
+export type TAB_KEYS = (typeof TAB_KEYS)[keyof typeof TAB_KEYS]
--- src/pages/question/edit/RightPanel.tsx
+++ src/pages/question/edit/RightPanel.tsx
@@ -1,8 +1,19 @@
 import { Tabs } from 'antd'
 import { FileTextOutlined, SettingOutlined } from '@ant-design/icons'
 import ComponentProp from './ComponentProp'
+import PageSetting from './PageSetting'
+import useGetComponentInfo from '../../../hooks/useGetComponentInfo'
+import { TAB_KEYS } from '../../../constant'
 
 const RightPanel = () => {
+  // const [activeKey, setActiveKey] = useState<TAB_KEYS>(TAB_KEYS.PROP_KEY)
+  const { selectedId } = useGetComponentInfo()
+  const activeKey = selectedId ? TAB_KEYS.PROP_KEY : TAB_KEYS.SETTING_KEY
+  // useEffect(() => {
+  //   if (selectedId) setActiveKey(TAB_KEYS.PROP_KEY)
+  //   else setActiveKey(TAB_KEYS.SETTING_KEY)
+  // }, [selectedId])
+
   const tabsItems = [
     {
       key: 'prop',
@@ -22,11 +33,11 @@ const RightPanel = () => {
           页面设置
         </span>
       ),
-      children: <div>页面设置</div>,
+      children: <PageSetting />,
     },
   ]
 
-  return <Tabs activeKey="prop" items={tabsItems}></Tabs>
+  return <Tabs activeKey={activeKey} items={tabsItems}></Tabs>
 }
 
 export default RightPanel
